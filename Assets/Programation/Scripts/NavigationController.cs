465c14dc379681070b4d412edb108efe0efdaa54
SanGameDev <santiago.gamedevs@gmail.com>
2022-11-26 12:45:12 -0600

    algo de avance
    emergenciaaaaa

--- Assets/Programation/Scripts/NavigationController.cs
+++ Assets/Programation/Scripts/NavigationController.cs
@@ -11,6 +11,10 @@ public class NavigationController : MonoBehaviour
     [Tooltip("Referencia en runtime a posicion del objetivo")]
     [SerializeField] Transform targetPosition;
 
+    private int vida = 10;
+
+    public TorreStatsSo torreStats;
+
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
@@ -19,6 +23,19 @@ public class NavigationController : MonoBehaviour
     {
         targetPosition = GameObject.Find("Target").GetComponent<Transform>();
         agent.SetDestination(targetPosition.position);
+    }
+
+    private void Update() 
+    {
+        if(vida <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //vida - torreStats.damage;
     }
     
 }
