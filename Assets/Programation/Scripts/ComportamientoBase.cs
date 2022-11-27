1a65e837b343aafb7f71a178718ae1c3d108dc65
Ketosvaldo <ketchulo.conk2013@gmail.com>
2022-11-26 18:23:22 -0600

    Completado

---
+++ Assets/Programation/Scripts/ComportamientoBase.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ComportamientoBase : MonoBehaviour
+{
+    GameObject[] tilesParaColocarBase;
+    int contadorDeOleadasProvisional = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilesParaColocarBase = GameObject.FindGameObjectsWithTag("Adyacente");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            contadorDeOleadasProvisional++;
+            MoverPosicion();
+        }
+    }
+
+    void MoverPosicion()
+    {
+        int random = Random.Range(0, tilesParaColocarBase.Length);
+        ColocarTorretaConMouse tile = tilesParaColocarBase[random].GetComponent<ColocarTorretaConMouse>();
+        if (!tile.est·Ocupado)
+            transform.position = tilesParaColocarBase[random].transform.position + new Vector3(0, 1, 0);
+        else
+            MoverPosicion();
+    }
+}
